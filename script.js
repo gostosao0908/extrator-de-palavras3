bcd7c7a47c5082258c5838bef84c9dc9087fcf0f
gostosao0908 <celestino.santos.felipe@escola.pr.gov.br>
2025-09-16 09:42:17 -0300

    gostosa 4

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ const botaomostraPalavras = document.querySelector("#botao-palavrachave");
 botaomostraPalavras.addEventListener("click", mostraPalavrasChave);
 
 function mostraPalavraChave() {
-    const texto = document.querySelector("#entrada-de-texto").value;
+    const texto = document.querySelector('#entrada-de-texto').value;
 
     const campoResultado = document.querySelector("#resultado-palavrachave")
 
 const palavraschave = processatexto(texto);
 
 
-campoResultado.textContent=palavras;.join(",");
+campoResultado.textContent = palavras.join(", ");
 
 function preocessatexto(texto) {
       let palavras = texto.split(/\P[L]+/U);
