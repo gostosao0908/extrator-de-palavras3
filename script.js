6c26960919607709c61f3817e94acbd4c600d5fc
gostosao0908 <celestino.santos.felipe@escola.pr.gov.br>
2025-09-23 09:40:46 -0300

    acabado aula 555555

--- script.js
+++ script.js
@@ -4,17 +4,25 @@ botaomostraPalavras.addEventListener('click', mostraPalavrasChave);
 
 function mostraPalavraChave() {
     const texto = document.querySelector('#entrada-de-texto').value;
-
     const campoResultado = document.querySelector('#resultado-palavrachave')
-
-const palavraschave = processatexto(texto);
+    const palavraschave = processatexto(texto);
 }
 
 campoResultado.textContent = palavras.join(", ");
 
 function preocessatexto(texto) {
-      let palavras = texto.split(/\P[L]+/U);
-      return palavras;
-      }
+    let palavras = texto.split(/\P[L]+/U);
 
-      
+let frequencias = ();
+for (let i of palavras) {
+    frequencia{i} =0;
+       for(let j of palavras) {
+           if(i==j) (
+               frequencia(i)++;
+             }
+         }
+
+ }
+
+        return palavras;
+)
